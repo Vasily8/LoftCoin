94507c7d6dfd6ab6603ebd524ac6bad262b42dc3
Vasily <gurin.vasili@yandex.ru>
2018-10-25 01:15:34 +0300

    homework_9

--- app/src/main/java/com/vasily/loftcoin/data/db/Database.java
+++ app/src/main/java/com/vasily/loftcoin/data/db/Database.java
@@ -1,6 +1,8 @@
 package com.vasily.loftcoin.data.db;
 
 import com.vasily.loftcoin.data.db.model.CoinEntity;
+import com.vasily.loftcoin.data.db.model.Transaction;
+import com.vasily.loftcoin.data.db.model.TransactionModel;
 import com.vasily.loftcoin.data.db.model.Wallet;
 import com.vasily.loftcoin.data.db.model.WalletModel;
 
@@ -14,9 +16,13 @@ public interface Database {
 
     void saveWallet(Wallet wallet);
 
+    void saveTransaction(List<Transaction> transactions);
+
     Flowable<List<CoinEntity>> getCoins();
 
     Flowable<List<WalletModel>> getWallets();
+
+    Flowable<List<TransactionModel>> getTransactions(String walletId);
 
     CoinEntity getCoin(String symbol);
 }
